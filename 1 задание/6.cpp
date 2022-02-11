31e0b3000199353cbef2a384902436cb08bacae9
DashaKrivosheina <91007535+DashaKrivosheina@users.noreply.github.com>
2022-02-11 15:50:57 +0300

    принадлежность точки кругу

---
+++ 1 задание/6.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+void func(float x, float y, float a, float b, float r)
+{
+    if ((x - a) * (x - a) + (y - b) * (y - b) <= r * r)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "No";
+    }
+}
+
+int main()
+{
+    float x, y, a, b, r;
+    cout << "Enter point coordinates: ";
+    cin >> x >> y;
+    cout << "Enter circle coordinates and radius: ";
+    cin >> a >> b >> r;
+    func(x, y, a, b, r);
+}
+
