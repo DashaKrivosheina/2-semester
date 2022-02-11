5105db611b93e2f33b487000d2d3bd6ba4ff39a7
DashaKrivosheina <91007535+DashaKrivosheina@users.noreply.github.com>
2022-02-11 12:44:37 +0300

    число пар студентов стоящих не по росту

---
+++ 1 задание/3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int func(int N)
+{
+    cout << "Enter an height: ";
+    int current = 0;
+    cin >> current;
+    int height = current;
+    --N;
+    int pair = 0;
+    for (int i = 0; i < N; i++)
+    {
+        cout << "Enter an height: ";
+        cin >> current;
+        if (current > height)
+        {
+            pair++;
+        }
+    }
+    return pair;
+}
+
+int main()
+{
+    int Pair, n;
+    cout << "Enter number of students ";
+    cin >> n;
+    Pair = func(n);
+    cout << "The number od wrong pair: " << Pair;
+}
