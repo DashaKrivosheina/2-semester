96223647611f0bba20ad747e89c3ee326b78fbc1
DashaKrivosheina <91007535+DashaKrivosheina@users.noreply.github.com>
2022-03-04 12:22:33 +0300

    Add files via upload

---
+++ task 2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+void func() {
+    int a;
+    cin >> a;
+    if (a != 0) {
+        int b;
+        cin >> b;
+        cout << a << " ";
+        if (b != 0) {
+            func();
+        }
+    } 
+}
+
+int main()
+{
+    func();
+}
