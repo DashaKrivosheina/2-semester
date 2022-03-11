20df46c74b01c61193b4db7cec1dae8a585d91e6
DashaKrivosheina <91007535+DashaKrivosheina@users.noreply.github.com>
2022-03-11 13:30:20 +0300

    Add files via upload

---
+++ 2 задание/task 4.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+void func(int &a, int &n) {
+    int c;
+    cin >> c;
+    if (c != 0) {
+        if (c > a) {
+            a = c;
+            n = 1;
+        }
+        else if (c == a) {
+            n++;
+        }
+        func(a, n);
+    }
+}
+
+int main()
+{
+    int a = 0;
+    int n = 0;
+    func(a, n);
+    cout << n;
+}
