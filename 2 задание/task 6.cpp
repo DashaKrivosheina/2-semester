479dbd3cf69003e41fa6ace3f62316e41227590e
DashaKrivosheina <91007535+DashaKrivosheina@users.noreply.github.com>
2022-03-11 14:08:13 +0300

    Add files via upload

---
+++ 2 задание/task 6.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int mfunc(int n);
+
+int ffunc(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    else {
+        return n - mfunc(ffunc(n - 1));
+    }
+}
+
+int mfunc(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    else {
+        return n - ffunc(mfunc(n - 1));
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << ffunc(n) << " " << mfunc(n);
+}
