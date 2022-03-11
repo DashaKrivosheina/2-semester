0f652f326393ab4df6aafb4b65ebf00c05208ac1
DashaKrivosheina <91007535+DashaKrivosheina@users.noreply.github.com>
2022-03-11 12:10:46 +0300

    Add files via upload

---
+++ 2 задание/task5.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int func(int a, int &b) {
+    if (a / 10 != 0) {
+        int c = func(a / 10, b);
+        b *= 10;
+        return (a % 10) * b + c;
+
+    }
+    else {
+        return a;
+    }
+}
+
+int main()
+{
+    int a;
+    int b = 1;
+    cin >> a;
+    cout << func(a, b);
+    return 0;
+}
