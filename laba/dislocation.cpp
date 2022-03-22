650522073d92174fef55664e526f16e459d9dda3
DashaKrivosheina <91007535+DashaKrivosheina@users.noreply.github.com>
2022-03-22 09:10:59 +0300

    Add files via upload

---
+++ laba/dislocation.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+using namespace std;
+
+//генерация рандомных чисел
+float random(int k) {
+    float f = (rand() % k);
+    return f;
+}
+
+void func() {
+    srand(static_cast<unsigned int>(time(0)));;
+    for (unsigned int m = 0; m < 501; ++m) {
+        for (int k = 0; k < 400; ++k) {
+            ofstream outf("task11.txt", ios::app);
+            const int n = 500;
+            int A[n][n];
+            for (unsigned int i = 0; i < m; ++i) {
+                for (unsigned int j = 0; j < m; ++j) {
+                    A[i][j] = 1;
+                }
+            }
+            int x = m / 2;
+            int y = m / 2;   
+            A[x][y] = 0;
+            bool end = 0;
+            for (int step = 0; !end; ++step) {
+                if (x == 0 || x == m - 1 || y == 0 || y == m - 1) {
+                    outf << m << " " << step << "\n";
+                    end = 1;
+                }
+                else {
+                    int v = random(4);
+                    if (v == 0) {
+                        x++;
+                    }
+                    if (v == 1) {
+                        x--;
+                    }
+                    if (v == 2) {
+                        y++;
+                    }
+                    if (v == 3) {
+                        y--;
+                    }
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    func();
+}
---
+++ laba/dislocations.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+using namespace std;
+
+//генерация рандомных чисел
+int random(int k) {
+    int f = (rand() % k);
+    return f;
+}
+
+int func() {
+    srand(static_cast<unsigned int>(time(0)));
+    ofstream outf("task2.txt", ios::app);
+    const int n = 400;
+    const int d = 200;
+    int A[n][n];
+    int D[d][d];
+    int step = -1;
+    for (int j = 1; j < d + 1; ++j) {//количество дислокаций
+        for (int k = 0; k < 100; ++k) { //количество повторений для каждого значения дислокаций
+            for (int i = 0; i < n; ++i) {
+                for (int l = 0; l < n; ++l) {
+                    A[i][l] = 1;
+                }
+            }
+            step = -1;
+            int t = 0;
+            while (t < j) {
+                int x = random(n);
+                int y = random(n);
+                if (A[x][y] == 1) {
+                    A[x][y] = 0;
+                    D[t][0] = x;
+                    D[t][1] = y;
+                    ++t;
+                }
+            }
+            int motion = t;
+            while (motion != 0) {
+                for (int f = 0; f < t; ++f) {// проверка на стоянку
+                    if (A[D[f][0]][D[f][1]] != 2 && ((D[f][0] == 0) || (D[f][0] == n - 1) || (D[f][1] == 0) || (D[f][1] == n - 1))) {
+                        A[D[f][0]][D[f][1]] = 2; // дислокация больше не передвигается
+                        motion--;
+                    }
+                }
+                for (int a = 0; a < t; ++a) {
+
+                    if (A[D[a][0]][D[a][1]] == 0) {
+                        if (A[D[a][0] + 1][D[a][1]] != 1 || A[D[a][0] - 1][D[a][1]] != 1 || A[D[a][0]][D[a][1] + 1] != 1 || A[D[a][0]][D[a][1] - 1] != 1) {
+                            A[D[a][0]][D[a][1]] = 2;
+                            motion--;
+                        }
+                        else {
+                            int v = random(4);
+                            if (v == 0) {
+                                D[a][0]++;
+                                A[D[a][0]][D[a][1]] = 0;
+                                A[D[a][0] - 1][D[a][1]] = 1;
+                            }
+                            if (v == 1) {
+                                D[a][0]--;
+                                A[D[a][0]][D[a][1]] = 0;
+                                A[D[a][0] + 1][D[a][1]] = 1;
+                            }
+                            if (v == 2) {
+                                D[a][1]++;
+                                A[D[a][0]][D[a][1]] = 0;
+                                A[D[a][0]][D[a][1] - 1] = 1;
+                            }
+                            if (v == 3) {
+                                D[a][1]--;
+                                A[D[a][0]][D[a][1]] = 0;
+                                A[D[a][0]][D[a][1] + 1] = 1;
+                            }
+                        }
+                    }
+
+                }
+                step++;  
+            }
+            outf << j << " " << step << "\n";
+        }    
+    } 
+    return step;
+}
+
+int main()
+{
+    func();
+}
