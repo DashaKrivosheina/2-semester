efe0ec1e3ac9aa396e4fea0d5be6fb83ac00e274
DashaKrivosheina <91007535+DashaKrivosheina@users.noreply.github.com>
2022-03-28 21:37:13 +0300

    Add files via upload

---
+++ 3 задание/3t.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+void my_personal_swap(int* c, int* d) {
+	if (c == nullptr || d == nullptr) {
+		return;
+	}
+	int x;
+	x = *d;
+	*d = *c;
+	*c = x;
+}
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	my_personal_swap(&a, &b);
+	cout << a << " " << b << endl;
+	my_personal_swap(&a, nullptr);
+	my_personal_swap(nullptr, &b);
+	my_personal_swap(nullptr, nullptr);
+	return 0;
+}
